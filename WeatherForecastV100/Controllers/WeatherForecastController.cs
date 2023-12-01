32d2b153b7b41e847e555868acaa4fe0faec1611
Kitesalet <104630744+Kitesalet@users.noreply.github.com>
2023-12-01 11:39:53 -0300

    Modificacion en la ruta del controller

--- WeatherForecastV100/Controllers/WeatherForecastController.cs
+++ WeatherForecastV100/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace WeatherForecastV100.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
